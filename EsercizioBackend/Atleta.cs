9be426bfbdd0e5086ca561b2feed92e7ec6b19ee
GiampyLux <giampaolopaladino01@gmail.com>
2024-06-17 15:37:24 +0200

    Aggiungere i file di progetto.

---
+++ EsercizioBackend/Atleta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioBackend
+{
+    internal class Atleta
+    {
+        string nome;
+        string cognome;
+        string sport;
+
+
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Cognome
+        {
+            get { return cognome; }
+            set { cognome = value; }
+        }
+
+        public string Sport {
+            get { return sport; }
+            set { sport = value; }
+        }
+
+        public void description()
+        {
+            Console.WriteLine("Mi chiamo " + nome + " " + cognome + "e pratico " + sport);
+        }
+    }
+}
---
+++ EsercizioBackend/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace EsercizioBackend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Atleta a = new Atleta();
+            a.Nome = "Giovanni";
+            a.Cognome = "De Blasi";
+            a.Sport = "Padel";
+            a.description();
+        }
+    }
+}
